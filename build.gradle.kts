ea8517eabc86e4cb7164ea5bab0aa29712739463
Thalles <thalles_jacobs@hotmail.com>
2024-09-18 22:08:15 -0300

    added sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("org.sonarqube") version "5.0.0.4638"
 }
 
 group = "com.knowledge"
@@ -25,6 +26,13 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "ci-project")
+		property("sonar.projectName", "ci-project")
+	}
 }
 
 kotlin {
