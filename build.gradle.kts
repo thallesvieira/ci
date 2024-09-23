4b7543000b0864638b3b7e2aaacf229f1507d8bd
Thalles <thalles_jacobs@hotmail.com>
2024-09-23 14:03:37 -0300

    sonar clound

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,6 @@ sonarqube {
 		property("sonar.organization", "thallesvieira")
 		property("sonar.host.url", "https://sonarcloud.io")
  		property("sonar.token", "9787d3f54a50b8d49eef4f79a0aca7790e715335")
-//		property("sonar.token", System.getenv("SONAR_TOKEN"))
 		property("sonar.coverage.jacoco.xmlReportPaths", "$buildDir/reports/jacoco/test/jacocoTestReport.xml")
 	}
 }
